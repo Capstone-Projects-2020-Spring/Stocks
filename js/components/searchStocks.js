0fb715013abbbe5732dc70ae794f33e30f5c3a80
Dan Burger <danburger@Dans-MacBook-Pro.local>
2020-03-16 01:04:49 -0400

    added graph examples to search stock page

--- js/components/searchStocks.js
+++ js/components/searchStocks.js
@@ -7,8 +7,9 @@ function searchStocks(id){
     
    </br> Stock Symbol: <input type="text" id="searchTicker"/>
          
-           <input type="button" value="Submit"/>
+           <input type="button" value="Submit" onclick="searchStocks.search('searchTicker','stockPage')"/>
 
+           <div id="stockPage"></div> 
 </div>
     
     
@@ -20,3 +21,20 @@ function searchStocks(id){
     document.getElementById(id).innerHTML = content;
     
 }
+
+
+
+searchStocks.search = function (ticker, targetId) {
+    var contentDOM = document.getElementById(targetId);
+    console.log(document.getElementById(ticker).value);
+    if (document.getElementById(ticker).value === ("AAPL")){
+        contentDOM.innerHTML = "<img src='./Images/AAPL.png' width='500' height='400'>";
+    }else if(document.getElementById(ticker).value === ("FB")){
+        contentDOM.innerHTML = "<img src='./Images/FB.png' width='500' height='400'>"
+    }else if(document.getElementById(ticker).value === ("MSFT")){
+        contentDOM.innerHTML = "<img src='./Images/MSFT.png' width='500' height='400'>"
+    }else{
+        contentDOM.innerHTML = "There is currently no information on that stock."
+    }
+    ;
+};
