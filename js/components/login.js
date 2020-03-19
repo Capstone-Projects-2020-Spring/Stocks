03296c30098af1d690f735cd465fd6f3499028e9
tuh41428 <46900863+tuh41428@users.noreply.github.com>
2020-03-19 14:23:06 -0400

    Update login.js
    remove some test lines

--- js/components/login.js
+++ js/components/login.js
@@ -17,13 +17,11 @@ function login(id) {
 }
 
 function onResponse(response) {
-    console.log("We've entered on response function");
     var u = document.getElementById("errMessage");
     if (response.status === 204) {
         // open page, will later take to loginSuccess page then auto redirect to profile page
         window.open("#/loginSuccess", "_self");
     } else {
-        console.log("should display error message");
         // display error
         u.innerHTML = "Invalid username or password.";
     }
@@ -32,7 +30,6 @@ function onResponse(response) {
 login.loginSubmit = function(){
     var username = document.getElementById('username');
     var password = document.getElementById('password');
-    console.log("enter loginSubmit function " + document.getElementById('username').value + document.getElementById('password').value);
     if (document.getElementById('username').value === 'admin' && document.getElementById('password').value === 'admin') {
         console.log("success login");
         onResponse({ status: 204 });
