01dfe2e1bcc6ab1303f4baee341d7f17f3651825
FayazD <fayazud.din@temple.edu>
2020-03-28 01:21:46 -0400

    add drop down menu for help icon, add contact page, made edits to some styling

--- Web Pages/js/components/contactPage.js
+++ Web Pages/js/components/contactPage.js
@@ -1,7 +1,9 @@
 function contactPage(id){
     var content = `
-        
-    <div id="contactBox">
+    
+    <div id="contactForm">
+    <h1 style="text-align: center">Contact Us!</h1>
+    <div id="contactParagraph">
 
     Most of the answers to your questions can be found in the FAQs section of this page. However, if that is not the case then we are here to
     assist you with any other questions. Send us an email at example@temple.edu and we will get back to you as soon as possible. Make sure to
@@ -10,5 +12,17 @@ function contactPage(id){
 
     </div>
     
-    ';
+        <form id="contactFormContent">
+            <label for="email">Email</label><br>
+            <input type="text" name="email" id="contactEmail"><br>
+            <label for="question">Question</label><br>
+            <input type="text" name="question" id="contactQuestion"><br>
+            <button class="buttons" id="regButton">Send</button>
+        </form>
+    </div>
+    
+    
+    
+    `;
+    document.getElementById(id).innerHTML = content;
 }
