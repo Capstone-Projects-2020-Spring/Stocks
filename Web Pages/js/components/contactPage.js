13807f5317d4c6f126ed2e602be66d9bea3f65df
FayazD <fayazud.din@temple.edu>
2020-03-27 22:39:07 -0400

    missied some files in last commit - Revamped website style to make it more user friendly and added ability for users to register a new account that is stored on Firebase

---
+++ Web Pages/js/components/contactPage.js
@@ -0,0 +1,14 @@
+function contactPage(id){
+    var content = `
+        
+    <div id="contactBox">
+
+    Most of the answers to your questions can be found in the FAQs section of this page. However, if that is not the case then we are here to
+    assist you with any other questions. Send us an email at example@temple.edu and we will get back to you as soon as possible. Make sure to
+    put the question in the subject line and give a brief description of the problem that troubles you. One of our support team members will
+    respond to the email as soon as they can. We thank you for your patience.
+
+    </div>
+    
+    ';
+}
---
+++ Web Pages/js/components/register.js
@@ -0,0 +1,55 @@
+//learned user registration from https://www.youtube.com/watch?v=wkdCpktUfGg --> will modify to fit webstie
+function register(id) {
+
+    var content = `
+    <div id="registerForm">
+    
+        <form id="formContent">
+            <label for="email">Email</label><br>
+            <input type="text" name="email" id="regEmail"><br>
+            <label for="password">Password</label><br>
+            <input type="text" name="password" id="regPassword"><br>
+            <button class="buttons" id="regButton">Register</button>
+        </form>
+    
+    </div>
+    `;
+    document.getElementById(id).innerHTML = content;
+
+    const registerForm = document.querySelector('#formContent');
+    registerForm.addEventListener('submit', (e) =>{
+        //prevents page from refreshing when info is submitted
+        e.preventDefault();
+        const email = registerForm['regEmail'].value;
+        const password = registerForm['regPassword'].value;
+
+        auth.createUserWithEmailAndPassword(email, password).then(cred=>{
+            console.log(cred)
+            registerForm.reset();
+        })
+
+    })
+
+    // const userId = document.getElementById('userId');
+    // const firstName = document.getElementById('firstName');
+    // const lastName = document.getElementById('lastName');
+    // const email = document.getElementById('email');
+    // const addBtn = document.getElementById('addBtn');
+    // const updateBtn = document.getElementById('updateBtn');
+    // const removeBtn = document.getElementById('removeBtn');
+    //
+    // const database = firebase.database();
+    //
+    //
+    // addBtn.addEventListener('click', (e)=>{
+    //     e.preventDefault();
+    //     database.ref('/users/' + userId.value).set({
+    //         first_name: firstName.value,
+    //         last_name: lastName.value,
+    //         email: email.value
+    //     });
+    // });
+
+}
+
+
