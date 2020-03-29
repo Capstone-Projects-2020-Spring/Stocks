cda3a9244b8ddf641a1bf0d058712c7333bbeab7
zachy3 <tuj60685@temple.edu>
2020-03-28 21:19:05 -0400

    added csvs and edited main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-# Testing
-
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 import time
 
 from alpha_vantage.timeseries import TimeSeries
@@ -11,7 +10,7 @@ from pprint import pprint
 alpha_key = 'YNY88LER76HPO0G3'
 
 # list of stocks to retrieve
-stock_list = ['MSFT', 'AAPL', 'AMD', 'FB', 'SHOP', 'ROKU', 'ENPH', 'LYFT']
+stock_list = ['MSFT', 'AAPL', 'AMD', 'FB', 'ROKU', 'ENPH', 'LYFT', 'AAL']
 
 
 def retrieve(stock_list):
@@ -28,23 +27,34 @@ def retrieve(stock_list):
             ts = TimeSeries(key=alpha_key, output_format='pandas')
             data, meta_data = ts.get_daily(symbol=stock, outputsize='full')
 
-            #showing dataframe to make sure printing proper data
-            print(data.head(), '\n')
+            plot_graph(stock, data)
 
-            #length to see number of days back
+            print(data.head(), '\n')
             print(len(data))
 
-            ## function to convert to csv file
             convert_csv(data, stock)
 
             count += 1
+
         else:
             time.sleep(60)
             count = 0
 
 
 def convert_csv(data, stock):
-    data.to_csv('../StockCSV/' + stock + '.csv')
+    data.to_csv('../Stocks/Stock_CSV/' + stock + '.csv')
+
+
+def plot_graph(stock, data):
+    to_plot = plt.plot(data['4. close'], color='darkolivegreen')
+    # to see the inline plotting - unneccessary now
+    # to_plot.plot()
+    # plt.color
+    plt.xlabel('Years', color='firebrick')
+    plt.ylabel('Prices', color='firebrick')
+    plt.title(stock + ': Prices over Time', color='darkblue')
+    plt.savefig('../Stocks/Stock_Plot/' + stock + '.png')
+    plt.show()
 
 
 def main():
