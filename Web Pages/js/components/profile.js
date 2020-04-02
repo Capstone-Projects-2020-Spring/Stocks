f67a6df267a42eee46e048e1871b567743db0bea
FayazD <fayazud.din@temple.edu>
2020-04-02 19:22:55 -0400

    drop down menu update

--- Web Pages/js/components/profile.js
+++ Web Pages/js/components/profile.js
@@ -1,13 +1,9 @@
 function profile(id) {
 
     auth.onAuthStateChanged(user => {
-        const loginLink = document.getElementById('login');
-        const regLink = document.getElementById('register');
 
 
         if(user){
-            loginLink.style.display = 'none';
-            regLink.style.display = 'none';
             var content = `
     <div class="profileContainer">
         <div class="Profile">
@@ -20,8 +16,6 @@ function profile(id) {
     `;
             document.getElementById(id).innerHTML = content;
         } else{
-            loginLink.style.display = 'inline-block';
-            regLink.style.display = 'inline-block';
             var content = `
     <div class="profileContainer">
         <div class="Profile"> You are logged out!</div>
